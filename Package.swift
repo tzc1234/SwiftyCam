e64a2e8d5b34369ac368582bc90056e10d8c5ac4
tzc1234 <peopli9997@gmail.com>
2022-03-20 13:59:45 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,19 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "SwiftyCam",
+            targets: ["SwiftyCam"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftyCam")
+    ],
+    swiftLanguageVersions: [.v5]
 )
